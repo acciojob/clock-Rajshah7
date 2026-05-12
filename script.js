77079e92130325a86a71f63232aae50eb1b252f6
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-12 22:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const element = document.getElementById("timer");
+
+let currentDate = new Date();
+
+setInterval(() => {
+	currentDate = new Date();
+	element.textContent = currentDate.toLocaleString();
+},1000)
